0a0e648bdfcf69b589c323e8596cf67577f953f8
ariesunique <ariesunique@gmail.com>
2020-02-06 21:20:50 -0500

    Chapter 8 - added followers and unit testing

--- app/models.py
+++ app/models.py
@@ -5,6 +5,12 @@ from flask_login import UserMixin
 from hashlib import md5
 
 
+    
+followers = db.Table('followers', 
+                     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')), 
+                     db.Column('followed_id', db.Integer, db.ForeignKey('user.id')))
+    
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -13,6 +19,28 @@ class User(UserMixin, db.Model):
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     posts = db.relationship("Post", backref="author", lazy="dynamic")
+    
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+    
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+            
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+            
+    def is_following(self, user):
+        return self.followed.filter(followers.c.followed_id == user.id).count() > 0
+    
+    def followed_posts(self):
+        followed_posts = Post.query.join(followers, (followers.c.followed_id == Post.user_id)).filter(followers.c.follower_id == self.id)                
+        own_posts = Post.query.filter_by(user_id=self.id)
+        return followed_posts.union(own_posts).order_by(Post.timestamp.desc())
     
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -36,8 +64,8 @@ class Post(db.Model):
     
     def __repr__(self):
         return "<Post {}>".format(self.body)
-    
-    
+
+
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
