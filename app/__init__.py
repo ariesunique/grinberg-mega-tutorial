4a03e1b683972c7b74babd2c193adcd86ecd20fe
ariesunique <ariesunique@gmail.com>
2020-02-11 11:20:33 -0500

    Trivial - reordered import statements

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from config import Config
 import logging
 import os
 from logging.handlers import SMTPHandler, RotatingFileHandler
